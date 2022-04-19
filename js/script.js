3e76a5946144db8faa27b4293b6b260f88447ecb
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-19 14:42:26 +0000

    Added calculations in JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
 // Created by: Maria G
 // Created on: April 19, 2022
 // This file contains the JS functions for index.html
+
+function temperatureEntered() {
+  let fahrenheit = parseFloat(document.getElementById("temp").value);
+  let celsius = 5 / 9 * (fahrenheit - 32);
+  document.getElementById("results").innerHTML = "The temperature in Celsius is " + celsius.toFixed(2) + " degrees Celsius."
+}
